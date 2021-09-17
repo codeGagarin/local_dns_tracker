ccd8d0b2df78c4d36646acbe120816f88697e4ab
igor <belov78@gmail.com>
2021-09-17 22:14:06 +0300

    + resolve code

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import dns.resolver
 
-def main():
-    answers = dns.resolver.query('station-hotels.ru', dns.rdatatype.A)
-    for rdata in answers:
-        print('Host', 'has preference', rdata)
+from keys import Keys
 
-# Press the green button in the gutter to run the script.
+
+class Alarmer:
+    def alarm(self, name: str):
+        print(f'Follow name as not resolved: {name}')
+
+    def send(self):
+        pass
+
+
+def resolve(name: str):
+    try:
+        dns.resolver.query(name, dns.rdatatype.A)
+    except dns.resolver.NXDOMAIN:
+        return False
+    return True
+
+
+def main():
+    alarmer = Alarmer()
+    for sub_name in Keys.SRV_LIST:
+        full_name = f'{sub_name}.{Keys.DOMAIN}'
+        if not resolve(full_name):
+            alarmer.alarm(full_name)
+        alarmer.send()
+
+
+
+
+
 if __name__ == '__main__':
     main()
